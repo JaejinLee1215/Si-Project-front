beae8a099ea20a3b61f40ff4dcbef73f549a6b71
jyeongvv <kjy812467@gmail.com>
2023-07-28 17:21:10 +0900

    navbar 수정

--- src/components/navigation/NavBar.js
+++ src/components/navigation/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './NavBar.css';
-import HomeIcon from '@mui/icons-material/AccessAlarm';
+import HomeIcon from '@mui/icons-material/Home';
 
 const NavBar = () => {
   return (
