67107dfd4af512cde831fc8534eb8f532489adce
jyeongvv <kjy812467@gmail.com>
2023-07-28 20:59:42 +0900

    category

--- src/components/category/Category.js
+++ src/components/category/Category.js
@@ -1,18 +1,56 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
 
-const Category = () => (
-  <div>
-    <div className="category-container1">
-      <div className="category category-1 buttons">
-        <button className="fill">Fill In</button>
-      </div>
-    </div>
-    <div className="category-container2">
-      <div className="category category-2 buttons">
-        <button className="fill">Click me</button>
-      </div>
-    </div>
-  </div>
-);
+const Category = () => {
+  return (
+    <Box
+      className="cate-box-container"
+      sx={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: '280px',
+      }}
+    >
+      <Paper
+        elevation={2}
+        className="cat-paper-item"
+        sx={{
+          margin: '30px',
+          width: 270,
+          height: 200,
+          background: 'white',
+          borderRadius: 10,
+          border: '2px solid #6AAF5F',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Typography variant="h6" sx={{ fontSize: '20px' }}>이혼😵‍💫</Typography>
+      </Paper>
+      <Paper
+        elevation={2}
+        className="cat-paper-item"
+        sx={{
+          margin: '30px',
+          width: 270,
+          height: 200,
+          background: '',
+          borderRadius: 10,
+          border: '2px solid #6AAF5F',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Typography variant="h6" sx={{ fontSize: '20px' }}>상속👦🏻</Typography>
+      </Paper>
+    </Box>
+  );
+};
 
 export default Category;
--- src/components/main/home.js
+++ src/components/main/home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './home.css';
 import NavBar from '../navigation/NavBar';
 import Search from '../search/Search';
+import Category from '../category/Category';
 
 const Home = () => {
     return (
         <div>
             <NavBar />
             <Search />
+            <Category />
         </div>
     )
 }
